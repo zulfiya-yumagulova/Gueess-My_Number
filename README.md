412a2c1ad0c64bcf014015fe2cc5b8293cf8a338
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2022-07-20 12:33:55 +0100

    started implement game logic

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Gueess-My_Number
+
+Welcome to my game "Guess My Number!"
--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log(document.querySelector(".message"));
+const checkBtn = document.querySelector(".check");
+const guessInput = Number(document.querySelector(".guess").value);
+const againBtn = document.querySelector(".again");
+const message = document.querySelector(".message");
+let randomNumber = Math.floor(Math.random() * 20 + 1);
+const secretNumber = document.querySelector(".secret-number");
+console.log(randomNumber);
+
+secretNumber.textContent = randomNumber;
+
+checkBtn.addEventListener("click", function () {
+  if (!guessInput) {
+    message.textContent = "No Number!";
+  }
+  if (guessInput === randomNumber) {
+    message.textContent = "Correct!";
+  }
+});
