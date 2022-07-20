875eff7ae6af204c50ae89167f04492816b62641
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2022-07-20 16:42:47 +0100

    if statement works

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 "use strict";
 const checkBtn = document.querySelector(".check");
-const guessInput = Number(document.querySelector(".guess").value);
 const againBtn = document.querySelector(".again");
 const message = document.querySelector(".message");
 let randomNumber = Math.floor(Math.random() * 20 + 1);
@@ -10,10 +9,18 @@ console.log(randomNumber);
 secretNumber.textContent = randomNumber;
 
 checkBtn.addEventListener("click", function () {
+  const guessInput = Number(document.querySelector(".guess").value);
   if (!guessInput) {
     message.textContent = "No Number!";
-  }
-  if (guessInput === randomNumber) {
+    console.log("no number");
+  } else if (guessInput === randomNumber) {
     message.textContent = "Correct!";
+    console.log("correct");
+  } else if (guessInput > randomNumber) {
+    message.textContent = "Too high!";
+    console.log("too high");
+  } else if (guessInput < randomNumber) {
+    message.textContent = "Too low";
+    console.log("too low");
   }
 });
